6d227f56b52ecd8af8d2353e43490151ef94a7b7
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-09-23 08:01:09 +0600

    Добавьте файлы проекта.

---
+++ HW-Task7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW_Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int[] DeleteItems(int[] array, int number)
+        {
+            for(int i = 1; i < number; i++)
+            {
+                Console.WriteLine($"Введите удаляемое число номер {i}");
+                
+            }
+        }
+    }
+}
---
+++ OOP-HW-1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOP_HW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = new int[10] { 8, 4, 2, 7, 5, 10, 1, 9, 3, 6 };
+
+            var arrayClone = (int[])array1.Clone(); 
+
+            var array2 = array1;
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                array2[i] += SortDesc(arrayClone)[i];
+            }
+
+            foreach (int num in array2)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int[] SortDesc(int[] array)
+        {
+            int temp;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
